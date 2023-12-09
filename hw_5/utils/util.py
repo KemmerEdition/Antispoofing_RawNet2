d829c7984e2d608a4c059f5926f5a5d081167c10
Anastasia Kemmer <anastasiya_kemmer@mail.ru>
2023-12-09 15:50:46 +0300

    fix grad

--- hw_5/utils/util.py
+++ hw_5/utils/util.py
@@ -66,8 +66,8 @@ class MetricTracker:
             self._data[col].values[:] = 0
 
     def update(self, key, value, n=1):
-        # if self.writer is not None:
-        #     self.writer.add_scalar(key, value)
+        if self.writer is not None:
+            self.writer.add_scalar(key, value)
         self._data.total[key] += value * n
         self._data.counts[key] += n
         self._data.average[key] = self._data.total[key] / self._data.counts[key]
