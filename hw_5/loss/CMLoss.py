e169acee0acef3dc64385e65f8e057b3a293a876
Anastasia Kemmer <anastasiya_kemmer@mail.ru>
2023-12-12 00:09:07 +0300

    abl change ce weights

--- hw_5/loss/CMLoss.py
+++ hw_5/loss/CMLoss.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 class CMLoss(nn.Module):
     def __init__(self):
         super().__init__()
-        self.ce_loss = nn.CrossEntropyLoss(weight=torch.tensor([1.0, 9.0]))
+        self.ce_loss = nn.CrossEntropyLoss(weight=torch.tensor([1.0, 1.0]))
 
     def forward(self, predicts, target, **kwargs):
         loss = self.ce_loss(predicts, target)
